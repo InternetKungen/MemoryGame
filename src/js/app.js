eb2f21930f274c4e1040e3858ed1d7be38a1b165
Kim Andersson <101129778+InternetKungen@users.noreply.github.com>
2023-11-23 01:26:02 +0100

    fixes

--- src/js/app.js
+++ src/js/app.js
@@ -13,38 +13,51 @@
 
 const emojis = ["🌼", "🌼", "🚗", "🚗", "🤡", "🤡", "🐵", "🐵", "💎", "💎", "💣", "💣", "🍟", "🍟", "🍄", "🍄"];
 
-// Shuffle emojis
+// Shuffle emojis - tar emojis array och ändrar ordningen
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5) ? 1 : -1);
 
-// Assuming you have a div with the class 'game' in your HTML
+// Hittar .game och binder platsen till variabel gameContainer
 const gameContainer = document.querySelector('.game');
 
+//Lägga ut korten efter shuffleEmojus-array
 for (let i = 0; i < shuffleEmojis.length; i++) {
+    //skapa en variablel för att skapa div-element
     let box = document.createElement('div');
+    //lägg till class "item" till div-elementet
     box.className = 'item';
+    //fyller div med emoji från shuffleEmojis array. --kan ändras till innerContent
     box.innerHTML = shuffleEmojis[i];
-
+    //On-click på box/div-elementet...
     box.onclick = function() {
+        //lägg till class 'boxOpen' på div-elementet
         this.classList.add('boxOpen');
+        //En timeout, som flippar korten efter andra har visats
         setTimeout (function() {
+            //Om antal divs med classen boxOpen är med än 1, alltså så gäller detta vid att andra kortet visas..
             if(document.querySelectorAll('.boxOpen').length > 1){
+                //Om korten är lika..
                 if(document.querySelectorAll('.boxOpen')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML){
+                    
+                    //Lägg till classen 'boxMatch' på par
                     document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch');
                     document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch')
 
+                    //Ta bort classen 'boxOpen' från korten
                     document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen');
-                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
+                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen');
 
+                    //Om alla korten som har classen 'boxMatch', så är spelet över -- emoji.length ger det totala antalet kort, shuffleEmojis.length fungerar också.
                     if(document.querySelectorAll('.boxMatch').length == emojis.length) {
-                        alert('You get em all!')
+                        alert('You got em all!');
                     }
                 } else {
+                    //Annars - Flippa tillbaka korten
                     document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen');
-                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
+                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen');
 
                 }
             }
-        }, 500)
+        }, 500) //timeOut
     }
 
     gameContainer.appendChild(box);
